0ca5006a2eedc0ca37d8c3ad464dbc3a11819f19
Aaron Janse <gitduino@gmail.com>
2016-10-25 16:08:39 -0700

    test code

--- arm_test.c
+++ arm_test.c
@@ -1,3 +1,9 @@
 task main() {
-  motor[armBase] = vexRT[Ch1]
+  //motor[armBase1] = vexRT[Ch1]
+slaveMotor(armBase2, armBase1);
+  setMotorTarget( armBase1, 50, 64, true );
+
+// Wait for motor to move
+while(!getMotorTargetCompleted(armBase1))
+    wait1Msec(10);
 }
